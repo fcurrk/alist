3fd4fc35c30ec61bb067a7bc0d0f08e74ca5d81f
fcurrk <48197714+fcurrk@users.noreply.github.com>
2023-01-20 15:30:58 +0800

    update

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ gitAuthor=$(git show -s --format='format:%aN <%ae>' HEAD)
 gitCommit=$(git log --pretty=format:"%h" -1)
 gitTag=$(git describe --long --tags --dirty --always)
 
-version="3.9.0.1"
-webVersion="3.9.0.1"
+version="3.9.1.1"
+webVersion="3.9.1.1"
 
 echo "backend version: $version"
 echo "frontend version: $webVersion"
