0286f3836165ec8d68e92ff6c3926f55d8d1eb7b
fcurrk <48197714+fcurrk@users.noreply.github.com>
2023-12-01 14:47:51 +0800

    Update 3.29.1.1201

--- build.sh
+++ build.sh
@@ -9,8 +9,8 @@ if [ "$1" = "dev" ]; then
   version="dev"
   webVersion="dev"
 else
-  version="3.29.1.1127"
-  webVersion="3.29.1.1127"
+  version="3.29.1.1201"
+  webVersion="3.29.1.1201"
 fi
 
 echo "backend version: $version"
