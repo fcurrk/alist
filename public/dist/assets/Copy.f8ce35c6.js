5fcb4269cf7214a0fe2932020853dd03a6c22fd1
fcurrk <48197714+fcurrk@users.noreply.github.com>
2024-01-21 13:30:51 +0800

    Update 3.30.0.0121

--- public/dist/assets/Copy.f8ce35c6.js
+++
@@ -1,1 +0,0 @@
-import{h as e}from"./index.c1a63d86.js";import{n as o}from"./index.8c955772.js";import{T as t}from"./Tasks.584968bd.js";import"./Paginator.d764e51c.js";import"./index.ab049ea3.js";const s=()=>(o("manage.sidemenu.copy"),e(t,{type:"copy",canRetry:!0}));export{s as default};
---
+++ public/dist/assets/ImageWithError.a081cfb0.js
@@ -0,0 +1,1 @@
+import{d as n,h as e,ai as o,at as s,S as c}from"./index.c3138b80.js";const l=r=>{const[a,t]=n(!1);return e(c,{get when(){return!a()},get fallback(){return r.fallbackErr},get children(){return e(o,s(r,{onError:()=>{t(!0)}}))}})};export{l as I};
