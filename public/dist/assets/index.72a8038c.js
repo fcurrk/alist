b9a4e29ed66b2d63094eb531f05f07c8bbcf93fe
fcurrk <48197714+fcurrk@users.noreply.github.com>
2025-02-04 16:10:23 +0800

    Update 3.42.0.250205

--- public/dist/assets/index.72a8038c.js
+++
@@ -1,1 +0,0 @@
-import{h as t,ac as e,a7 as r}from"./index.6a11a750.js";import n from"./Upload.1eb8f1e7.js";import"./index.97e8bcc5.js";const m=()=>t(r,{justifyContent:"center",h:"100vh",get children(){return t(e,{w:"$md",get children(){return t(n,{})}})}});export{m as default};
